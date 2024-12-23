e52ec63d583e485b7ca00e9cef2375e701fb7900
mayur7267 <mayurlate.7267@gmail.com>
2024-12-23 12:04:18 +0530

    Initial Commit

---
+++ assignment1/assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment1App.swift
+//  assignment1
+//
+//  Created by Mayur on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ assignment1Tests/assignment1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  assignment1Tests.swift
+//  assignment1Tests
+//
+//  Created by Mayur on 23/12/24.
+//
+
+import Testing
+@testable import assignment1
+
+struct assignment1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
